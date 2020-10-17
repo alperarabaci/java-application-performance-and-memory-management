2d337c168393ab377f1a8402779536544762588d
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-10-17 16:54:48 +0300

    Renamed, Chapter 9 added.

---
+++ chapter9-exploringStrings/src/main/Main.java
@@ -0,0 +1,40 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//40. The String Pool
+		String one = "hello";
+		String two = "hello";
+
+		System.out.println(one.equals(two));
+		System.out.println(one == two);
+		System.out.println("---------------");
+
+		Integer i = 76;
+		String three = i.toString();
+		String four = "76";
+
+		System.out.println(three.equals(four));
+		// this return false:
+		/**
+		 * now this line of code three equals four produced a false it's possible at
+		 * some point in the future that line of code could result in a true because of
+		 * this internal optimization that the virtual machine can do. It hasn't done it
+		 * yet in this instance but it could do it in the future to say ah I've got two
+		 * strings here that have the same underlying value I can remove one of them and
+		 * safely point both able references to the same single object on the heap
+		 */
+		System.out.println(three == four);
+		System.out.println("---------------");
+
+		//41. Interning Strings
+		Integer j = 76;
+		String five = j.toString().intern();
+		String six = "76";
+
+		System.out.println(five.equals(six));
+		System.out.println(five == six);
+	}
+
+}
